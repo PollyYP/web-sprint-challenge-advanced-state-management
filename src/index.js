f112823981a968b32bf7a3802436e6cf12593ee0
Polly Yospan <porgroovy@gmail.com>
2021-03-07 12:16:32 -0500

    create store, install redux-logger, add thunk and logger as middleware, fetch data using axios in action file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
 
 import "./index.css";
 import App from "./App";
+import { reducer } from "./reducers/index";
 
-const { worker } = require('./mocks/browser');
+const { worker } = require("./mocks/browser");
 worker.start();
+
+const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-    <App />, 
-    rootElement
+  <Provider>
+    {" "}
+    store={store}
+    <App />
+  </Provider>,
+  rootElement
 );
 
 //Task List:
